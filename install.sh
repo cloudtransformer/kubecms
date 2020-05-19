3e96ad99757aa1abcc5be13ba22231be22b7c951
Dan Lister <d.lister@agebase.co.uk>
2020-05-19 22:50:41 +0100

    Added more contents to install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 set -e
 
+GITHUB_URL=https://github.com/kubecms/kubecms
+GITHUB_VERSION=master
+
 # --- helper functions for logs ---
-
-info()
-{
-    echo '[INFO] ' "$@"
-}
-
-warn()
-{
-    echo '[WARN] ' "$@" >&2
-}
 
 fatal()
 {
@@ -28,6 +21,50 @@ verify_system()
     fatal 'Could not find kubectl to use as a provisioner of Kube CMS'
 }
 
+# --- verify existence of network downloader executable ---
+verify_downloader() 
+{
+    # Return failure if it doesn't exist or is no executable
+    [ -x "$(which $1)" ] || return 1
+
+    # Set verified executable as our downloader program and return success
+    DOWNLOADER=$1
+    return 0
+}
+
+# --- create tempory directory and cleanup when done ---
+setup_tmp() 
+{
+    TMP_DIR=$(mktemp -d -t kubecms-install)
+
+    cleanup() 
+    {
+        code=$?
+        set +e
+        trap - EXIT
+        rm -rf ${TMP_DIR}
+        exit $code
+    }
+
+    trap cleanup INT EXIT
+}
+
+# --- download binary from github url ---
+download_binary() 
+{
+    ZIP_URL=${GITHUB_URL}/archive/${GITHUB_VERSION}.zip
+    info "Downloading zip ${ZIP_URL}"
+}
+
+# --- download and verify kubecms ---
+download_and_verify() 
+{
+    verify_downloader curl || verify_downloader wget || fatal 'Could not find curl or wget for downloading files'
+    setup_tmp
+    download_binary
+}
+
 {
     verify_system
+    download_and_verify
 }
